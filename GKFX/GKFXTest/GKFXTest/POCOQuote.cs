d986c9bdad58fc7c9b052f7f3dfc9878d5112e4d
SergeyM <serg.al.melnikov@gmail.com>
2016-06-02 00:01:56 +0300

    commit 2

---
+++ GKFX/GKFXTest/GKFXTest/POCOQuote.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GKFXTest
+{
+    class POCOQuote
+    {
+        public DateTime Time { get; set; }
+        public string Sym { get; set; }
+        public decimal BidPrice { get; set; }
+        public decimal AskPrice { get; set; }
+    }
+}
---
+++ GKFX/GKFXTest/GKFXTest/POCOTrade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GKFXTest
+{
+    class POCOTrade
+    {
+        public DateTime Time { get; set; }
+        public string Sym { get; set; }
+        public Side Side { get; set; }
+        public uint Amount { get; set; }
+        public decimal Price { get; set; }
+    }
+}
