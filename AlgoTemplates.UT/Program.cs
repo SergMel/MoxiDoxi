172b6e6157bf621c185fe05e63e5a3b630114acf
serg <serg.al.melnikov@gmail.com>
2018-10-27 17:48:46 +0100

    crab graph

--- AlgoTemplates.UT/Program.cs
+++ AlgoTemplates.UT/Program.cs
@@ -1,19 +1,60 @@
 ﻿using AlgoTest;
 using System;
+using System.IO;
 
 namespace AlgoTemplates.UT
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-			FFTTest.RunTests();
-			DijkstraMinHeapTest.RunTest();
-			MaxSegTreeTest.RunTest();
-			HeapTest.RunTests();
-			GCDTest.RunTests();
 
-			Console.WriteLine("Hello World!");
-        }
-    }
+		// Complete the solve function below.
+		static double solve(int[][] coordinates)
+		{
+			long minx = long.MaxValue;
+			long miny = long.MaxValue;
+			long maxx = long.MinValue;
+			long maxy = long.MinValue;
+			foreach (var lst in coordinates)
+			{
+				if (minx > lst[0]) minx = lst[0];
+				if (maxx < lst[0]) maxx = lst[0];
+				if (miny > lst[1]) miny = lst[1];
+				if (maxy < lst[1]) maxy = lst[1];
+			}
+			var q1 = Math.Max(Math.Abs(minx), Math.Abs(maxx));
+			var q2 = Math.Max(Math.Abs(miny), Math.Abs(maxy));
+			return Math.Max(Math.Max(maxx - minx, maxy - miny), Math.Sqrt((double)q1 * q1 + (double)q2 * q2));
+
+		}
+
+		static void Main(string[] args)
+		{
+			FileStream stream = new FileStream(@"C:\Personal\tmp\hr.txt", FileMode.Open);
+			StreamReader sr = new StreamReader(stream);
+			int n = Convert.ToInt32(sr.ReadLine());
+
+			int[][] coordinates = new int[n][];
+
+			for (int coordinatesRowItr = 0; coordinatesRowItr < n; coordinatesRowItr++)
+			{
+				coordinates[coordinatesRowItr] = Array.ConvertAll(sr.ReadLine().Split(' '), coordinatesTemp => Convert.ToInt32(coordinatesTemp));
+			}
+			sr.Close();
+			stream.Close();
+			double result = solve(coordinates);
+
+			Console.WriteLine(result);
+		}
+		//     static void Main(string[] args)
+		//     {
+		////FFTTest.RunTests();
+		////DijkstraMinHeapTest.RunTest();
+		////MaxSegTreeTest.RunTest();
+		////HeapTest.RunTests();
+		////GCDTest.RunTests();
+		//Crab_graphs.Crab_graphs.Main(args);
+		//// Crab_graphsTests.BFSTest();
+		//Console.WriteLine("Hello World!");
+		//     }
+	}
 }
--- AlgoTemplates/Crab-graphs.cs
+++ AlgoTemplates/Crab-graphs.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace AlgoTemplates.Crab_graphs
 {
+
 	public class Edge
 	{
 		public int Start { get; set; }
@@ -18,7 +19,7 @@ namespace AlgoTemplates.Crab_graphs
 	{
 		public Dictionary<int, List<Edge>> Edges;
 		public int VCount => Edges.Count;
-		
+
 
 		public FlexGraph()
 		{
@@ -29,7 +30,7 @@ namespace AlgoTemplates.Crab_graphs
 		{
 			if (!Edges.ContainsKey(edge.Start))
 				Edges[edge.Start] = new List<Edge>();
-			Edges[edge.Start].Add(edge); 
+			Edges[edge.Start].Add(edge);
 		}
 
 		public void RemoveVertex(int id)
@@ -78,8 +79,8 @@ namespace AlgoTemplates.Crab_graphs
 
 	}
 
-    class Crab_graphs
-    {
+	public class Crab_graphs
+	{
 		public static Pathes BFS(ResidualGraph graph, int start, int end)
 		{
 			Queue<int> q = new Queue<int>();
@@ -89,24 +90,26 @@ namespace AlgoTemplates.Crab_graphs
 			pathLength[start] = 0;
 			prev[start] = new List<Edge>();
 			int curLength = 0;
-			while(q.Count > 0 )
+			while (q.Count > 0)
 			{
 				var itm = q.Dequeue();
 				if (pathLength[end] > -1 && pathLength[end] < curLength)
 					break;
 
-				var nbrs = graph.Edges[itm]?.Where(el=>el.Capacity > 0)??Enumerable.Empty<Edge>();
+				var nbrs = graph.Edges[itm]?.Where(el => el.Capacity > 0) ?? Enumerable.Empty<Edge>();
 				foreach (var item in nbrs)
 				{
-					if (pathLength[item.End] >=0)
+					if (pathLength[item.End] >= 0 && pathLength[item.End] < pathLength[item.Start] + 1)
 						continue;
+					if (pathLength[item.End] < 0)
+						q.Enqueue(item.End);
 					pathLength[item.End] = pathLength[item.Start] + 1;
 					curLength = pathLength[item.End];
 					(prev[item.End] = prev[item.End] ?? new List<Edge>()).Add(item);
-					q.Enqueue(item.End);
+				
 				}
 			}
-			return pathLength[end] == -1?(Pathes)null: new Pathes
+			return pathLength[end] == -1 ? (Pathes)null : new Pathes
 			{
 				Lengths = pathLength,
 				Preious = prev
@@ -132,9 +135,10 @@ namespace AlgoTemplates.Crab_graphs
 					throw new InvalidOperationException();
 				foreach (var edge in itms)
 				{
-					if (!ret.Edges.ContainsKey(edge.Start) && pathes.Lengths[edge.End] == pathes.Lengths[edge.Start] + 1)
+					if (!ret.Edges.ContainsKey(edge.Start) || pathes.Lengths[edge.End] == pathes.Lengths[edge.Start] + 1)
 					{
-						q.Enqueue(edge.Start);
+						if (!ret.Edges.ContainsKey(edge.Start))
+							q.Enqueue(edge.Start);
 						ret.AddEdge(edge);
 					}
 				}
@@ -148,13 +152,13 @@ namespace AlgoTemplates.Crab_graphs
 			int f = 0;
 			do
 			{
-				f = Dinic(graph, start, end);
+				f = Dinic(graph, start, end, int.MaxValue);
 				flow += f;
 			} while (f > 0);
-			return flow;	
+			return flow;
 		}
 
-		public static int Dinic(FlexGraph graph, int start, int end, int flow )
+		public static int Dinic(FlexGraph graph, int start, int end, int flow)
 		{
 			if (start == end)
 				return flow;
@@ -164,10 +168,84 @@ namespace AlgoTemplates.Crab_graphs
 			{
 				var f = Dinic(graph, item.End, end, Math.Min(item.Capacity, flow));
 				if (f > 0)
+				{
+					item.Capacity -= f;
+					item.Paired.Capacity += f;
 					return f;
+				}
 			}
 			graph.RemoveVertex(start);
 			return 0;
 		}
-    }
+
+		public static int FullAlg(ResidualGraph graph, int start, int end)
+		{
+			Pathes p = null;
+			int flow = 0;
+			while ((p = BFS(graph, start, end)) != null)
+			{
+				var fg = BuildLGraph(p, graph, start, end);
+				var res = Dinic(fg, start, end);
+				flow += res;
+			}
+			return flow;
+		}
+
+
+		/*
+		 * Complete the crabGraphs function below.
+		 */
+		static int crabGraphs(int n, int tt, int[][] graph)
+		{
+			/*
+			 * Write your code here.
+			 */
+			ResidualGraph residualGraph = new ResidualGraph(2 * n + 2);
+			int s = 0; int t = 1;
+			for (int i = 0; i < graph.Length; i++)
+			{
+				residualGraph.AddEdge(2 * graph[i][0], 2 * graph[i][1] + 1, 1);
+				residualGraph.AddEdge(2 * graph[i][1], 2 * graph[i][0] + 1, 1);
+			}
+			for (int i = 1; i <= n; i++)
+			{
+				residualGraph.AddEdge(0, 2 * i, tt);
+				residualGraph.AddEdge(2 * i + 1, 1, 1);
+
+
+			}
+			var res = FullAlg(residualGraph, 0, 1) ;
+			return res;
+
+		}
+
+		public static void Main(string[] args)
+		{
+						int c = Convert.ToInt32(Console.ReadLine());
+
+			for (int cItr = 0; cItr < c; cItr++)
+			{
+				string[] ntm = Console.ReadLine().Split(' ');
+
+				int n = Convert.ToInt32(ntm[0]);
+
+				int t = Convert.ToInt32(ntm[1]);
+
+				int m = Convert.ToInt32(ntm[2]);
+
+				int[][] graph = new int[m][];
+
+				for (int graphRowItr = 0; graphRowItr < m; graphRowItr++)
+				{
+					graph[graphRowItr] = Array.ConvertAll(Console.ReadLine().Split(' '), graphTemp => Convert.ToInt32(graphTemp));
+				}
+
+				int result = crabGraphs(n, t, graph);
+
+				Console.WriteLine(result);
+			}
+
+			
+		}
+	}
 }
